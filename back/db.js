e4e95d8f25096df9678e07f855a0407f0ac2ffea
Tracy Ye <tye50@stuy.edu>
2025-05-09 11:44:04 -0400

    add

--- back/db.js
+++ back/db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3');
 
-function connect() {
-  const db = new sqlite3.Database("./database");
+async function connect() {
+  const db = await new sqlite3.Database("./database");
   db.run("CREATE TABLE IF NOT EXISTS rooms(user_id TEXT, room_id TEXT, role TEXT)");
   return db
 }
@@ -10,7 +10,7 @@ function close(db) {
   db.close();
 }
 
-function pick_role(in_room) { // returns a random untaken role
+export function pick_role(in_room) { // returns a random untaken role
   const chance = 1/(5-in_room.length)
   if (!in_room.includes("mafia")) {
       if (Math.random() <= chance) {
@@ -30,16 +30,33 @@ function pick_role(in_room) { // returns a random untaken role
   return "innocent";
 }
 
-async function get_in_room(db, room) { // returns an array of roles in a given room
+// https://stackoverflow.com/questions/65454450/how-to-assign-a-variable-to-the-output-of-a-sqlite-query-in-node-js
+export async function get_roles_in_room(room) { // returns an array of roles in a given room
+  const db = connect();
   return new Promise((resolve, reject) => {
     db.all("SELECT role FROM rooms WHERE room_id = ?", [room], (err, rows) => {
       if (err) {
         console.log(err.message);
+        reject(err);
       }
       resolve(rows);
     });
+    close(db);
   });
 }
+// example of calling get_roles_in_room:
+// const in_room = await get_roles_in_room(connect(), room);
 
-// example of calling get_in_room:
-// const in_room = await get_in_room(connect(), room);
+export async function add_to_room(user_id, room_id, role) { // basically, Promise takes in a functin and waits for the function to finish
+  const db = connect();
+  return new Promise((resolve, reject) => {
+    db.run("INSERT INTO rooms (user_id, room_id, role) VALUES (?, ?, ?)", [user_id, room_id, role], (err) => {
+      if (err) {
+        console.log(err.message);
+        reject(err);
+      }
+      resolve(console.log(`Inserted ${user_id} into room ${room_id} as ${role}`));
+    });
+    close(db);
+  });
+}
