729557462a7ebfc85d8eb296178f3f436c03e19d
Tracy Ye <tye50@stuy.edu>
2025-05-14 06:20:30 -0400

    just gotta fix the reduce error

--- back/db.js
+++ back/db.js
@@ -1,26 +1,52 @@
+// const sqlite3 = require('sqlite3');
 import sqlite3 from 'sqlite3';
 
-async function connect() {
-  const db = async() => {
-    return await new sqlite3.Database("./database");
-  };
-  const s = await db();
-  console.log(s);
-  // await db.run("CREATE TABLE IF NOT EXISTS rooms(user_id TEXT, room_id TEXT, role TEXT)");
-  const c = async() => {
-    return await db.all("SELECT role FROM room");
-  };
-  console.log(`${c} keojdeiadeeee`);
-  return db
+function connect() {
+  const db = new sqlite3.Database('database.db', (err) => {
+        if (err) {
+          console.log(err.message);
+        }
+        // console.log('>>> Connected to the database.');
+      });
+  return db;
 }
 
-async function close(db) {
-  await db.close();
+function close(db) {
+  db.close((err) => {
+    if (err) {
+      console.log(err.message);
+    }
+    // console.log('>>> Database closed.')
+  });
 }
 
-export function pick_role(in_room) { // returns a random untaken role
+function createTable() {
+  const db = connect();
+  return new Promise((resolve, reject) => {
+    db.run("CREATE TABLE IF NOT EXISTS rooms(user_id TEXT, room_id TEXT, role TEXT)", (err) => {
+      if (err) {
+        console.log(err.message);
+        reject(err);
+      }
+      resolve(console.log('Table[s] added.'));
+    });
+    close(db);
+  });
+}
+
+function pick_role(in_room) { // returns a random untaken role
   const chance = 1/(5-in_room.length)
-  if (!in_room.includes("mafia")) {
+  if (structuredClone(in_room).reduce((count, current) => { // if max innocent, then only consider missing roles
+    if (current == "innocent") count++;
+    return count;
+  }) == 2) {
+    const only_consider=[];
+    if (!in_room.includes("mafia")) only_consider.push("mafia");
+    if (!in_room.includes("cop")) only_consider.push("cop");
+    if (!in_room.includes("doctor")) only_consider.push("doctor");
+    return only_consider[Math.floor(Math.random() * only_consider.length)]
+  }
+  else if (!in_room.includes("mafia")) {
       if (Math.random() <= chance) {
           return "mafia";
       }
@@ -38,24 +64,7 @@ export function pick_role(in_room) { // returns a random untaken role
   return "innocent";
 }
 
-// https://stackoverflow.com/questions/65454450/how-to-assign-a-variable-to-the-output-of-a-sqlite-query-in-node-js
-export async function get_roles_in_room(room) { // returns an array of roles in a given room
-  const db = connect();
-  return new Promise((resolve, reject) => {
-    db.all("SELECT role FROM rooms WHERE room_id = ?", [room], (err, rows) => {
-      if (err) {
-        console.log(err.message);
-        reject(err);
-      }
-      resolve(rows);
-    });
-    close(db);
-  });
-}
-// example of calling get_roles_in_room:
-// const in_room = await get_roles_in_room(connect(), room);
-
-export async function add_to_room(user_id, room_id, role) { // basically, Promise takes in a functin and waits for the function to finish
+function add_to_room(user_id, room_id, role) {
   const db = connect();
   return new Promise((resolve, reject) => {
     db.run("INSERT INTO rooms (user_id, room_id, role) VALUES (?, ?, ?)", [user_id, room_id, role], (err) => {
@@ -63,8 +72,55 @@ export async function add_to_room(user_id, room_id, role) { // basically, Promis
         console.log(err.message);
         reject(err);
       }
-      resolve(console.log(`Inserted ${user_id} into room ${room_id} as ${role}`));
+        resolve(console.log(`Inserted ${user_id} into room ${room_id} as ${role}`));
     });
     close(db);
   });
 }
+
+function get_roles_in_room(room) { // returns an array of roles in a given room
+  const db = connect();
+  return new Promise((resolve, reject) => {
+    db.all("SELECT role FROM rooms WHERE room_id = ?", [room], (err, rows) => {
+      if (err) {
+        console.log(err.message);
+        reject(err);
+      }
+      const ret = []
+      rows.forEach(value => ret.push(value.role));
+      resolve(ret);
+    });
+    close(db);
+  });
+}
+
+function remove_from_room(user_id) {
+  const db = connect();
+  return new Promise((resolve, reject) => {
+    db.run("DELETE FROM rooms WHERE user_id = ?", [user_id], (err) => {
+      if (err) {
+        console.log(err.message);
+        reject(err);
+      }
+      resolve(console.log(`Removed ${user_id}.`));
+    });
+    close(db);
+  });
+}
+
+
+const temp_room = "123";
+const temp_user_id = "aurum";
+await createTable();
+console.log(await get_roles_in_room(temp_room));
+await add_to_room(temp_user_id, temp_room, pick_role(await get_roles_in_room(temp_room)));
+console.log(await get_roles_in_room(temp_room));
+await add_to_room("a", temp_room, pick_role(await get_roles_in_room(temp_room)));
+console.log(await get_roles_in_room(temp_room));
+await add_to_room("b", temp_room, pick_role(await get_roles_in_room(temp_room)));
+console.log(await get_roles_in_room(temp_room));
+await add_to_room("c", temp_room, pick_role(await get_roles_in_room(temp_room)));
+console.log(await get_roles_in_room(temp_room));
+await add_to_room("d", temp_room, pick_role(await get_roles_in_room(temp_room)));
+console.log(await get_roles_in_room(temp_room));
+await remove_from_room(temp_user_id);
--- back/index.js
+++ back/index.js
@@ -1,12 +1,9 @@
-import express from 'express';
-var app = express();
-// const http = require("http"); // sqlite making things act funny >:/
-import http from 'http';
-// const cors = require("cors");
-import cors from "cors";
-// const { Server } = require("socket.io");
-import { Server } from "socket.io";
-import { get_roles_in_room, pick_role, add_to_room } from './db.js';
+const express = require("express"); // search up what express does...
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const sqlite3 = require("sqlite3");
 app.use(cors());
 
 const server = http.createServer(app);
@@ -17,20 +14,13 @@ const io = new Server(server, {
     },
 });
 
-io.on("connection", (socket) => { // whenever a connection to the serve is detected, grabs 'socket' or the information of the connection
+io.on("connection", (socket) => { // whenever a connection to the serve is detected, grabs 'socket' or the information of the connection 
     console.log(`User connected: ${socket.id}`); // every connection is given a unique id
                 //               like f-strings but in js...
 
     socket.on("join_room", (data) => { // whenever the function join_room is emitted from frontend
-        const in_room = get_roles_in_room(data); // returns array of roles in given room
-        if (in_room.length < 5) {
-            socket.join(data); // adds you to an arbitrary room; you can now emit messages to everyone in that room at once (like clumping...) (https://socket.io/docs/v3/rooms/)
-            const role = pick_role(in_room); // returns random role
-            add_to_room(socket.id, data, role);
-        }
-        // else stay in waiting room!!!
-        // right now waiting room does not exist
-        // so you will progress to chat room eitherway
+        socket.join(data); // adds you to an arbitrary room; you can now emit messages to everyone in that room at once (like clumping...) (https://socket.io/docs/v3/rooms/) 
+        console.log(`User with ID: ${socket.id} joined room: ${data}`)
     });
 
     socket.on("send_message", (data) => {
@@ -41,15 +31,6 @@ io.on("connection", (socket) => { // whenever a connection to the serve is detec
 
     socket.on("disconnect", () => {
         console.log("User disconnected: ", socket.id) // listens to disconnects from the server
-
-        // const db = new sqlite3.Database("./database");
-        // db.run("DELETE FROM rooms WHERE user_id = ?", [socket.id], function(err) {
-        //     if (err) {
-        //         console.log(err.message);
-        //     }
-        //     console.log(`Deleted ${socket.id}`);
-        //     db.close();
-        // });
     });
 });
 
