a3ae5ae88cb312ef9f5934c9e69a9d31a21cd476
Claire Song <csong50@stuy.edu>
2025-05-19 11:01:41 -0400

    narration start

---
+++ app/back/narration.js
@@ -0,0 +1,11 @@
+const background = [
+  "was taking a stroll around the neighborhood",
+  "was taking their dog for a walk",
+  "was running through a big sunny field",
+  "was doing cartwheels on the grass"
+];
+
+const action = [
+  "fell onto an upturned rake",
+  "tripped over someone's sharp pet rock"
+]
