1e0221822f5dcf669188bbe0b9e928c96b692ce4
Claire Song <csong50@stuy.edu>
2025-05-20 11:01:29 -0400

    narration

--- app/back/narration.js
+++ app/back/narration.js
@@ -9,3 +9,21 @@ const action = [
   "fell onto an upturned rake",
   "tripped over someone's sharp pet rock"
 ]
+
+const result = [
+  "and was left to marinate in their big pool of blood",
+  "and hit their head a little too hard"
+]
+
+const saved = [
+  "but then someone pushed them out of the way"
+]
+
+function randomInt(max) {
+  return Math.floor(Math.random()*max);
+}
+
+function script(user, death) {
+  var result = user;
+  result+=background[randomInt(4)]
+}
