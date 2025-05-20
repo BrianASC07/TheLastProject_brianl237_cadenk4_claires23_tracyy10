091638e646864de62780be4a4796c8a735d9697a
Tracy Ye <tye50@stuy.edu>
2025-05-20 04:55:57 -0400

    Night page done! Add in time & 'dawn'(?) next

--- front/src/Night.js
+++ front/src/Night.js
@@ -1,48 +1,107 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export function Night({ socket, username, room, role }) {
-    const [userList, setUserList] = useState([]);
-    const [target, setTarget] = useState("");
+  const [userList, setUserList] = useState([]);
+  const [target, setTarget] = useState("");
+  const [checkRole, setCheckRole] = useState(false);
+  const [roleDescription, setRoleDescription] = useState("");
 
-    const options = async() => {
-      await socket.emit("request_userList", room);
-    };
+  const options = async () => {
+    await socket.emit("request_userList", room);
+  };
 
-    socket.on("user_list", (data) => {
-      setUserList(data);
-    });
+  const message = () => {
+    if (target !== "") {
+      if (role === "mafia") {
+        return <p> You have selected {target} to kill. </p>
+      }
+      if (role === "doctor") {
+        return <p> You have selected {target} to protect. </p>
+      }
+      else {
+        return <p> You have selected {target} to investigate. </p>
+      }
+    }
+    else {
+      return <p> You are thinking of who to select... </p>
+    }
+  }
 
-    options();
-    // return (
-    //     <div>
-    //         <p>cats on cows</p>
-    //         <p>role : {role}</p>
-    //         <div>
-    //             <p> userList : </p>
-    //             { userList.map((username, index) => {
-    //                 return <p>{username}</p>
-    //             })}
-    //         </div>
-    //     </div>
-    // );
+  function description(role) {
+    setCheckRole(true);
+    if (role === "mafia") {
+      setRoleDescription("The mafia is the evil guy, blah blah blah, kill someone each night...");
+    }
+    else if (role === "doctor") {
+      setRoleDescription("The doctor is a pretty cool role, blah blah blah, grant invincibility to a person for a night...");
+    }
+    else if (role === "cop") {
+      setRoleDescription("The cop is cool i guess, blah blah blah, select someone to investigate each night to learn their role in the morning...");
+    }
+    else {
+      setRoleDescription("The innocent is a basic role... you have no special role at night. Fear not because there is power in numbers, pay attention to the others' behaviour and vote to condemn the suspicious in the morning!");
+    }
+  }
 
+  const constant = () => {
     return (
       <div>
-        <p> Select a target: </p>
-        <button onClick={() => setTarget("username")}> username </button>
-        { userList.map((username, index) => {
-          return <button onClick={() => setTarget(username)}> {username} </button>
-        })}
+        <div /* top */>
+          <p> You are the </p>
+          <p> { role } </p>
+          {["mafia", "doctor", "cop", "innocent"].map((role, index) => {
+            return <button onClick={ () => description(role) }> {role} </button>
+          })}
+          { checkRole ? (
+            <div>
+              <p> {roleDescription} </p>
+              <button onClick={ () => setCheckRole(false) }> x </button>
+            </div>
+          ):(
+            ""
+          )}
+          <p> Time left : </p>
+          {/* add in time logic */}
+          <p> *** Narration here  *** </p>
+          {/* add in narration logic */}
+        </div>
+        <div /* side */>
+          <p> Alive : </p>
+          {userList.map((username, index) => {
+            return <p>{username}</p>
+          })}
+          <p> Spectating : </p>
+          {/* add in spectator logic */}
+        </div>
       </div>
+    )
+  }
 
-        // if (role == "") { // special role
-        //   <div>
-        //     <p> no cats on cows </p>
-        //   </div>
-        // }
-        // else { // innocent
-        //
-        // }
+  socket.on("user_list", (data) => {
+    setUserList(data);
+  });
 
+  options();
+  if (["mafia", "doctor", "cop"].includes(role)) {
+    return ( // if special role
+      <div>
+        { constant() }
+        <p> Select a target: </p>
+        {userList.map((uname, index) => {
+          if (username !== uname || role === "doctor") {
+            return <button onClick={() => setTarget(uname)}> {uname} </button>
+          }
+          else { return "" }
+        })}
+        { message() }
+      </div>
     );
+  }
+  else {
+    return ( // innocent
+      <div>
+        { constant() }
+      </div>
+    );
+  }
 }
