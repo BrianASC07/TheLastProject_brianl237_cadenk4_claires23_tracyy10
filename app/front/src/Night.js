712b62c019b5843e9819592b5b0ca1b1dcfadabf
Brian Liu <bliu50@stuy.edu>
2025-05-20 10:27:30 -0400

    recombine

--- app/front/src/Night.js
+++ app/front/src/Night.js
@@ -1,48 +1,60 @@
-import React, { useEffect, useState } from "react";
+import './App.css';
+import { Chat } from './Chat.js';
+import io from 'socket.io-client'; // front end must be started ('npm start') !!!
+import { useState } from "react";
+import { Night } from './Night.js';
 
-export function Night({ socket, username, room, role }) {
-    const [userList, setUserList] = useState([]);
-    const [target, setTarget] = useState("");
+const socket = io.connect("http://localhost:3001"); // connects this to the backend
 
-    const options = async() => {
-      await socket.emit("request_userList", room);
-    };
+function App() {
+  const [username, setUsername] = useState(""); // useState keeps track of the updated state of the variable
+  const [room, setRoom] = useState(""); // ref below...
+  const [showChat, setShowChat] = useState(false);
+  const [role, setRole] = useState("");
 
-    socket.on("user_list", (data) => {
-      setUserList(data);
-    });
+  const joinRoom = () => {
+    if (username !== "" && room !== "") { // requirements to join
+      socket.emit("join_room", [room, username]) // calls join_room in backend and passes the room id
+      // setShowChat(true);
+    }
+  };
 
-    options();
-    // return (
-    //     <div>
-    //         <p>cats on cows</p>
-    //         <p>role : {role}</p>
-    //         <div>
-    //             <p> userList : </p>
-    //             { userList.map((username, index) => {
-    //                 return <p>{username}</p>
-    //             })}
-    //         </div>
-    //     </div>
-    // );
+  socket.on("do_not_join", (data) => {
+    if (data === false) setShowChat(true);
+  });
 
-    return (
-      <div>
-        <p> Select a target: </p>
-        <button onClick={() => setTarget("username")}> username </button>
-        { userList.map((username, index) => {
-          return <button onClick={() => setTarget(username)}> {username} </button>
-        })}
-      </div>
+  socket.on("set_role", (data) => {
+    setRole(data);
+  });
 
-        // if (role == "") { // special role
-        //   <div>
-        //     <p> no cats on cows </p>
-        //   </div>
-        // }
-        // else { // innocent
-        //
-        // }
-
-    );
+  return (
+    <div className="App">
+      {!showChat ? ( // if showChat is false, allow to join
+        <div className="joinChatContainer">
+          <h3> join chat </h3>
+          <input
+            type="text"
+            placeholder="Name..."
+            onChange={(event) => // takes in the event (the inputted text)
+            {
+              setUsername(event.target.value); // updates the variable setUsername with input
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}> join room </button>
+        </div>
+      ) : ( // else show the chat
+        // <Chat socket={socket} username={username} room={room} />
+        <Night socket={ socket } username={ username } room={ room } role={ role } />
+      )}
+    </div>
+  );
 }
+
+export default App;
