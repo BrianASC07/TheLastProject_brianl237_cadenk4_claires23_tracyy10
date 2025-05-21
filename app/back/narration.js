f8ecf51d332a2c03f7ceb979003ff39e94d60fa7
Claire Song <144632175+Claire36@users.noreply.github.com>
2025-05-20 21:32:32 -0400

    Update narration.js

--- app/back/narration.js
+++ app/back/narration.js
@@ -1,22 +1,29 @@
 const background = [
-  "was taking a stroll around the neighborhood",
-  "was taking their dog for a walk",
-  "was running through a big sunny field",
-  "was doing cartwheels on the grass"
+  " was taking a stroll around the neighborhood",
+  " was taking their dog for a walk",
+  " was running through a big sunny field",
+  " was doing cartwheels on the grass"
 ];
 
 const action = [
-  "fell onto an upturned rake",
-  "tripped over someone's sharp pet rock"
+  " fell onto someone's upturned rake",
+  " tripped over someone's pet rock",
+  " fell into a 6-foot pit that someone dug in the grass", 
+  " slipped on someone's banana peel",
+  " got attacked by someone's lawn mower",
+  " got run over by a motorcycle"
 ]
 
 const result = [
-  "and was left to marinate in their big pool of blood",
-  "and hit their head a little too hard"
+  " and was left to marinate in their big pool of blood",
+  " and slowly faded into nothingness",
+  " and never got up again"
 ]
 
 const saved = [
-  "but then someone pushed them out of the way"
+  ", but then someone pushed them out of the way at the last second, saving their life.", 
+  ', but then someone screamed "WATCH OUT!!!" just in time to save their life.', 
+  ", but then someone called their name, causing them to turn in the opposite direction and saving their life."
 ]
 
 function randomInt(max) {
@@ -24,6 +31,14 @@ function randomInt(max) {
 }
 
 function script(user, death) {
-  var result = user;
-  result+=background[randomInt(4)]
+  var res = user;
+  res+=background[randomInt(4)];
+  if (death) {
+    res+=" when they" + action[randomInt(6)];
+    res+=result[randomInt(3)];
+  } else {
+    res+=" when they almost" + action[randomInt(6)];
+    res+=saved[randomInt(3)];
+  }
+  return res;
 }
