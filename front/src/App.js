012fd668f815f422155aacf00d49a8820a1a3d68
Tracy Ye <tye50@stuy.edu>
2025-05-22 11:02:33 -0400

    waitingroom

--- front/src/App.js
+++ front/src/App.js
@@ -3,11 +3,12 @@ import { Chat } from './Chat.js';
 import io from 'socket.io-client'; // front end must be started ('npm start') !!!
 import { useState } from "react";
 import { Night } from './Night.js';
+import { WaitingRoom } from './WaitingRoom.js';
 
-const socket = io.connect("http://localhost:3001"); // connects this to the backend
-// const socket = io.connect("http://localhost:3000");
+// const socket = io.connect("http://localhost:3001"); // connects this to the backend
+const socket = io.connect("http://localhost:3000");
 
-export function App() {
+function App() {
   const [username, setUsername] = useState(""); // useState keeps track of the updated state of the variable
   const [room, setRoom] = useState(""); // ref below...
   const [showNight, setshowNight] = useState(false);
@@ -16,7 +17,7 @@ export function App() {
   const joinRoom = () => {
     if (username !== "" && room !== "") { // requirements to join
       socket.emit("join_room", [room, username]) // calls join_room in backend and passes the room id
-      // setshowNight(true);
+      setshowNight(true);
     }
   };
 
@@ -53,7 +54,7 @@ export function App() {
           <button onClick={joinRoom}> connect </button>
         </div>
       ) : ( // else enter night
-        <Night socket={ socket } username={ username } room={ room } role={ role } />
+        <WaitingRoom socket={ socket } username={ username } room={ room } role={ role } />
       )}
     </div>
   );
---
+++ front/src/WaitingRoom.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { Night } from './Night.js';
+
+export function WaitingRoom({ socket, username, room, role }) {
+  const [userList, setUserList] = useState([]);
+
+  const options = async () => {
+    await socket.emit("request_userList", room);
+  };
+
+  socket.on("user_list", (data) => {
+    setUserList(data);
+  });
+
+  if (userList.length() === 5) {
+    return <Night socket={ socket } username={ username } room={ room } role={ role } />
+  }
+
+  options();
+  return (
+    <div> cats over cows </div>
+  )
+}
