5fec5ef53862dde94950c0f74d7639a10e0d8161
Caden Khuu <117611429+cadenk0@users.noreply.github.com>
2025-05-23 10:43:48 -0400

    fix WaitingRoom.js

--- app/front/src/WaitingRoom.js
+++ app/front/src/WaitingRoom.js
@@ -1,1 +1,29 @@
 
+import React, { useEffect, useState } from "react";
+import { Night } from './Night.js';
+
+export function WaitingRoom({ socket, username, room, role }) {
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    socket.emit("request_userList", room);
+
+    const handleUserList = (data) => {
+      setUserList(data);
+    };
+
+    socket.on("user_list", handleUserList);
+
+    return () => {
+      socket.off("user_list", handleUserList);
+    };
+  }, [socket, room]);
+
+  if (userList.length === 5) {
+    return <Night socket={socket} username={username} room={room} role={role} />;
+  }
+
+  return (
+    <div> cats over cows </div>
+  );
+}
