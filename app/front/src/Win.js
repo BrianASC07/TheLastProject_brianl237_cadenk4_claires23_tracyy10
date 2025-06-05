dbf870c24ab9ef73671c8a4e50282d62de27d000
Brian Liu <109633886+BrianASC07@users.noreply.github.com>
2025-06-05 16:12:05 -0400

    Update Win.js

--- app/front/src/Win.js
+++ app/front/src/Win.js
@@ -1,12 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { JoinRoom } from './JoinRoom.js';
 
-export function Win({ socket, username, room, role }) {
+export function Win({socket, username, room}) {
+  const [seconds, setSeconds] = useState(5);
 
+  useEffect(() => { // timer ticks down every second
+    return () => {
+      setInterval(() => {
+        setSeconds(prevSeconds => prevSeconds - 1);
+      }, 1000); // 1000 ms -> s
+    }
+  }, []);
 
+  if (seconds <= 0) {
+    socket.emit("force_disconnect", [username, room]);
+    window.location.reload();
+    return <JoinRoom socket={socket}/>
+  }
 
   return (
     <div>
-      <p> YOU WON WHOMP WHOMP </p>
+      <p> win condition met </p>
+      <p> room closes in: </p>
+      <p> {seconds} </p>
     </div>
   )
 }
+
+export default Win
