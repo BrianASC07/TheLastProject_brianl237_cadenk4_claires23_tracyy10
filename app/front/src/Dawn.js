067f73476e7dbae51191fcbfad2f8bc9e4e92150
Claire Song <144632175+Claire36@users.noreply.github.com>
2025-06-05 13:02:23 -0400

    Update Dawn.js

--- app/front/src/Dawn.js
+++ app/front/src/Dawn.js
@@ -23,6 +23,136 @@ export function Dawn({ socket, username, room, role, spectator, seconds }) {
     }
   }, [mafiaTarget, doctorTarget]);
 
+  const Canvas = props => {
+  const canvaS = useRef(null);
+
+  useEffect(() => {
+    const updateCanvas = canvaS.current;
+    if (!updateCanvas) { // if null
+      return;
+    }
+    const context = updateCanvas.getContext('2d');
+
+    const image = new Image();
+    var img = [];
+    
+    console.log(narration);
+
+    if (narration.includes("stroll around the neighborhood") || narration.includes("running through a big sunny field") || narration.includes("scouting out possible picnic spots") || narration.includes("cheerfully frolicking") || narration.includes("grandmother's huge garden") || narration.includes("half marathon") || narration.includes("walking in the meadow")) {
+        img.push(background);
+    } else if (narration.includes("taking their dog for a walk")) {
+        img.push(dog);
+    } else if (narration.includes("build up their bug collection")) {
+        img.push(bug);
+    } else if (narration.includes("chase a pretty butterfly")) {
+        img.push(butterfly);
+    }
+
+    if (narration.includes("upturned rake")) {
+        img.push(rake);
+    } else if (narration.includes("pet rock")) {
+        img.push(rock);
+    } else if (narration.includes("puddle of oil")) {
+        img.push(oil);
+    } else if (narration.includes("banana peel")) {
+        img.push(banana);
+    } else if (narration.includes("skateboard")) {
+        img.push(skateboard);
+    } else if (narration.includes("pet duck")) {
+        img.push(duck);
+    } else if (narration.includes("thorny pet plant")) {
+        img.push(cactus);
+    } else if (narration.includes("empty soda can")) {
+        img.push(soda);
+    } else if (narration.includes("jack-o-lantern")) {
+        img.push(pumpkin);
+    } else if (narration.includes("piece of string")) {
+        img.push(string);
+    }
+
+    var dead = true;
+    if (narration.includes("pool of blood")) {
+        img.push(blood);
+    } else if (narration.includes("slowly decompose")) {
+        img.push(rot);
+    } else if (narration.includes("never got up")) {
+        img.push(dead);
+    } else if (narration.includes("freshly baked cookies")) {
+        dead = false;
+        img.push(cookie);
+    } else {
+        dead = false;
+        img.push(exclaim);
+    }
+
+    image.src = img[0];
+    console.log(img);
+    var intervalID = null;
+    var row = 0;
+    var col = 0;
+    var tracker = "background";
+
+    if (narration.includes("running") || narration.includes("chase") || narration.includes("marathon")) {
+      var speed = 75;
+    } else {
+      var speed = 300;
+    }
+    console.log(speed);
+
+    image.onload = function () {
+      if ((!done) && img.length!=1) {
+        setDone(true);
+        console.log("lalala");
+        intervalID = setInterval(animate, speed, 4, 3, 3);
+        // context.drawImage(image, 1, 1, 400, 400, 0, 0, 500, 500);
+      }
+    }
+
+    function animate(rows, cols, endCol) {
+        if (col === cols) {
+            col = 0;
+            row += 1;
+        }
+        console.log(row, col);
+        context.clearRect(0, 0, 500, 500);
+        context.drawImage(image, 0+480*col, 0+480*row, 480, 480, 0, 0, 500, 500);
+        if (row === (rows-1) && col === (endCol-1)) {
+            console.log("done w/ sheet");
+            clearInterval(intervalID);
+            if (tracker === "background") {
+                tracker = "action";
+                image.src=img[1];
+                image.onload = function() {
+                    row = 0;
+                    col = 0;
+                    if (dead) {
+                        intervalID = setInterval(animate, speed, 4, 4, 2);
+                    } else {
+                        intervalID = setInterval(animate, speed, 3, 4, 2);
+                    }
+                }
+            } else if (tracker === "action") {
+                tracker = "result";
+                speed = 300;
+                image.src=img[2];
+                image.onload = function() {
+                    row = 0;
+                    col = 0;
+                    if (dead) {
+                        intervalID = setInterval(animate, speed, 3, 2, 1);
+                    } else {
+                        intervalID = setInterval(animate, speed, 4, 3, 2);
+                    }
+                }
+            }
+        }
+        col += 1;
+    }
+  }, [narration]);
+
+  return <canvas ref={canvaS} {...props} width="500" height="500"/>
+}
+
   if (useOnce) { // grab the targets from backend once
     socket.emit("get_mafia", "");
     socket.emit("get_doctor", "");
